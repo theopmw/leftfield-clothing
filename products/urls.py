f84407ee9c63bc763c2a6fa671a73d34a97495f7
theopmw <theowrightdev@gmail.com>
2021-10-21 17:49:30 +0000

    Fix subcategory url path

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
+from django.urls import path, re_path
 from . import views
 
 # Code urls modified from:
 # https://betterprogramming.pub/optimizing-django-admin-6a1187ddbb09 &
 # https://stackoverflow.com/questions/47843241/django-admin-how-to-populate-select-options-depending-on-another-select
 app_name = 'products'
+
 urlpatterns = [
-    url(r'^/getSubcategory/$', views.get_subcategory)
+    re_path(r'^getSubcategory/$', views.get_subcategory)
     ]
