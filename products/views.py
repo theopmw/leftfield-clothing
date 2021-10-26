8fd6317ef398fdadb1de198e9a965ff9b4dbc3c4
theopmw <theowrightdev@gmail.com>
2021-10-26 13:48:23 +0000

    Comment out get_subcategory view, add all_products view

--- products/views.py
+++ products/views.py
@@ -4,19 +4,32 @@ from django.http import HttpResponse
 import json
 
 
-# Code for views modified from:
+# Code for view modified from:
 # https://betterprogramming.pub/optimizing-django-admin-6a1187ddbb09 &
 # https://stackoverflow.com/questions/47843241/django-admin-how-to-populate-select-options-depending-on-another-select
-def get_subcategory(request):
+# def get_subcategory(request):
+#     """
+#     View to get parent category, filter items in the subcategory field
+#     dropdown based on the category selected and respond with json data.
+#     """
+#     # Get category id from DB based on dropdown selection
+#     category = request.GET.get('id')
+#     print("CATEGORY: ", category)
+#     # Filter subcategories based on the category select box value
+#     result = list(SubCategory.objects.filter(
+#         category_id=int(category)).values('category', 'name'))
+#     print("GET RESULT: ", result)
+#     return HttpResponse(json.dumps(result), content_type="application/json")
+
+
+def all_products(request):
     """
-    View to get parent category, filter items in the subcategory field
-    dropdown based on the category selected and respond with json data.
+    View to show all products, including sorting and search queries.
     """
-    # Get category id from DB based on dropdown selection
-    category = request.GET.get('id')
-    print("CATEGORY: ", category)
-    # Filter subcategories based on the category select box value
-    result = list(SubCategory.objects.filter(
-        category_id=int(category)).values('category', 'name'))
-    print("GET RESULT: ", result)
-    return HttpResponse(json.dumps(result), content_type="application/json")
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
