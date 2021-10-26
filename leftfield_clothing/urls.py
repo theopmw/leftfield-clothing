54df64e608698545aa50176930e2fafc907846f9
theopmw <theowrightdev@gmail.com>
2021-10-26 14:09:52 +0000

    Add products templates and urls and project level urls

--- leftfield_clothing/urls.py
+++ leftfield_clothing/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
-    path('', include('products.urls')),
+    # re_path('', include('products.urls')),
+    path('products/', include('products.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/urls.py
+++ products/urls.py
@@ -5,8 +5,10 @@ from . import views
 # Code urls modified from:
 # https://betterprogramming.pub/optimizing-django-admin-6a1187ddbb09 &
 # https://stackoverflow.com/questions/47843241/django-admin-how-to-populate-select-options-depending-on-another-select
-app_name = 'products'
+
+# app_name = 'products'
 
 urlpatterns = [
-    re_path(r'^getSubcategory/$', views.get_subcategory)
+    path('', views.all_products, name='products'),
+    # re_path(r'^getSubcategory/$', views.get_subcategory),
     ]
