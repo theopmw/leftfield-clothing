c4c85c270aaaa58c2c82fefb052f7eafec3bf502
theopmw <theowrightdev@gmail.com>
2021-10-26 15:56:17 +0000

    Add product details page functionality

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product, SubCategory, Category
 from django.http import HttpResponse
 import json
@@ -33,3 +33,16 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """
+    View to show individual product details.
+    """
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
