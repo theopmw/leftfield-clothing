6e419fea54688c4a409fcad46c1980179d3df2ec
theopmw <theowrightdev@gmail.com>
2021-10-27 08:06:31 +0000

    Add product slugs to product urls

--- products/urls.py
+++ products/urls.py
@@ -10,6 +10,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<product_id>', views.product_detail, name='product_detail'),
+    path('<slug>/', views.product_detail, name='product_detail'),
     # re_path(r'^getSubcategory/$', views.get_subcategory),
     ]
--- products/views.py
+++ products/views.py
@@ -56,11 +56,11 @@ def all_products(request):
     return render(request, 'products/products.html', context)
 
 
-def product_detail(request, product_id):
+def product_detail(request, slug=None):
     """
     View to show individual product details.
     """
-    product = get_object_or_404(Product, pk=product_id)
+    product = get_object_or_404(Product, slug=slug)
 
     context = {
         'product': product,
