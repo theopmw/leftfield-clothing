bbc36419d0fcbdf2cf2bd153dcd3ddfe12f60f43
theopmw <theowrightdev@gmail.com>
2021-11-02 15:30:29 +0000

    Add docstring to calc_subtotal template filter

--- bag/templatetags/bag_tools.py
+++ bag/templatetags/bag_tools.py
@@ -1,8 +1,9 @@
 from django import template
 
-
 register = template.Library()
+
 
 @register.filter(name='calc_subtotal')
 def calc_subtotal(price, quantity):
+    """ Calculate subtotal """
     return price * quantity
