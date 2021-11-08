3ee594c79f625855eec02985c8ceafa9c4f9fb9c
theopmw <theowrightdev@gmail.com>
2021-11-08 15:42:40 +0000

    Update checkout form order and add Django CountyField select box

--- checkout/forms.py
+++ checkout/forms.py
@@ -21,12 +21,11 @@ class OrderForm(forms.ModelForm):
             'full_name': 'Full Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
-            'country': 'Country',
             'postcode': 'Postal Code',
             'town_or_city': 'Town or City',
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
-            'county': 'County',
+            'county': 'County, State or Locality',
         }
 
         # Set autofocus on full_name field to True so cursor starts in
@@ -34,15 +33,16 @@ class OrderForm(forms.ModelForm):
         self.fields['full_name'].widget.attrs['autofocus'] = True
         # Iterate through form fields
         for field in self.fields:
-            # Add a star to the placeholder if it's a required field
-            # on the model
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
-            else:
-                placeholder = placeholders[field]
-            # Set all the placeholder attributes to their values in the
-            # dictionary above
-            self.fields[field].widget.attrs['placeholder'] = placeholder
+            if field != 'country':
+                # Add a star to the placeholder if it's a required field
+                # on the model
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                # Set all the placeholder attributes to their values in the
+                # dictionary above
+                self.fields[field].widget.attrs['placeholder'] = placeholder
             # Add CSS class to fields: 'stripe-style-input'
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             # Remove form fields labels
