7746f0d7c97dc03e816d3449e02384a6db18840e
theopmw <theowrightdev@gmail.com>
2021-11-24 12:15:09 +0000

    Add blog PostList and PostDetail views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Post
+
+
+# Credit: code for blog views modified from:
+# https://djangocentral.com/building-a-blog-application-with-django/
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'blog/blog.html'
+
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
