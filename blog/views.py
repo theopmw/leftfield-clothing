4d5282d87518a4b625f2972ffb34572990f617e4
theopmw <theowrightdev@gmail.com>
2021-11-29 09:10:41 +0000

    Add docstrings to PostList and PostDetail views

--- blog/views.py
+++ blog/views.py
@@ -6,10 +6,16 @@ from .models import Post
 # Credit: code for blog views modified from:
 # https://djangocentral.com/building-a-blog-application-with-django/
 class PostList(generic.ListView):
+    """
+    View to show all blog posts
+    """
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'blog/blog.html'
 
 
 class PostDetail(generic.DetailView):
+    """
+    View to show blog post detail page
+    """
     model = Post
     template_name = 'blog/post_detail.html'
