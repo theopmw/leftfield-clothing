c39236b9dd9aca03898b44a6acb2500a03e17986
theopmw <theowrightdev@gmail.com>
2021-11-29 19:01:50 +0000

    Fix PEP8 compliance

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
-from . import views
 from django.urls import path
+from . import views
+
 
 # Credit: code for blog URLs modified from:
 # https://djangocentral.com/building-a-blog-application-with-django/
