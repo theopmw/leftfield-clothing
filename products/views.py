bc35220512aed4f27fca64f3e03dddffb19eb60c
theopmw <theowrightdev@gmail.com>
2021-11-30 08:37:28 +0000

    Remove unused code, fix PEP8 problems

--- products/views.py
+++ products/views.py
@@ -1,30 +1,10 @@
-import json
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.db.models.functions import Lower
-from django.http import HttpResponse
 from .models import Product, SubCategory, Category
 from .forms import ProductForm
-
-
-# Code for view modified from:
-# https://betterprogramming.pub/optimizing-django-admin-6a1187ddbb09 &
-# https://stackoverflow.com/questions/47843241/django-admin-how-to-populate-select-options-depending-on-another-select
-# def get_subcategory(request):
-#     """
-#     View to get parent category, filter items in the subcategory field
-#     dropdown based on the category selected and respond with json data.
-#     """
-#     # Get category id from DB based on dropdown selection
-#     category = request.GET.get('id')
-#     print("CATEGORY: ", category)
-#     # Filter subcategories based on the category select box value
-#     result = list(SubCategory.objects.filter(
-#         category_id=int(category)).values('category', 'name'))
-#     print("GET RESULT: ", result)
-#     return HttpResponse(json.dumps(result), content_type="application/json")
 
 
 def all_products(request):
@@ -41,6 +21,7 @@ def all_products(request):
     # Check if request.GET exists
     if request.GET:
         # Check if sort is in request.GET
+        # Sort products in the dropdown menu
         if 'sort' in request.GET:
             # If it is set it to sort and sortkey
             sortkey = request.GET['sort']
@@ -78,7 +59,6 @@ def all_products(request):
             subcategories = SubCategory.objects.filter(name__in=subcategories)
 
         # Check if 'q' is in request.GET
-        # (text input in the search form is named 'q')
         if 'q' in request.GET:
             query = request.GET['q']
             # If query is blank attach an error message to the request
