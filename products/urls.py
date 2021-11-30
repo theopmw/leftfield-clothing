f7c4f1bf1de8da8963faa1056a9d4ae204746f58
theopmw <theowrightdev@gmail.com>
2021-11-30 08:36:51 +0000

    Fix PEP8 problems

--- products/urls.py
+++ products/urls.py
@@ -1,12 +1,9 @@
-from django.conf.urls import url
-from django.urls import path, re_path
+from django.urls import path
 from . import views
 
 # Code urls modified from:
 # https://betterprogramming.pub/optimizing-django-admin-6a1187ddbb09 &
 # https://stackoverflow.com/questions/47843241/django-admin-how-to-populate-select-options-depending-on-another-select
-
-# app_name = 'products'
 
 urlpatterns = [
     path('', views.all_products, name='products'),
@@ -22,6 +19,5 @@ urlpatterns = [
     path(
         '<slug:slug>/<int:product_id>/', views.product_detail,
         name='product_detail'),
-    # re_path(r'^getSubcategory/$', views.get_subcategory),
     path('add/', views.add_product, name='add_product'),
     ]
